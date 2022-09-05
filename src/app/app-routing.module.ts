85adc952145b7dc4ed805e14d5b98bbaf2a7ac15
AlvaroArpal <alvaroarpal@gmail.com>
2022-09-05 11:30:43 +0200

    Setup basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch : 'full', redirectTo: 'list'},
+  {
+    path       : '',
+    children   : [
+        {path: 'list', loadChildren: () => import('./components/list/list.module').then(m => m.ListModule)},
+        {path: 'detail', loadChildren: () => import('./components/detail/detail.module').then(m => m.DetailModule)},
+      ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
